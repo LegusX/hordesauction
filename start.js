12d82d09c4a06522a14c9fba3c32553f73d3e500
Logan Henrie <loghenrie@gmail.com>
2020-05-11 18:49:05 -0500

    hmm

--- start.js
+++ start.js
@@ -1,5 +1,16 @@
-const express = require("express")
-const app = express()
+var express = require('express')
+var fs = require('fs')
+var https = require('https')
+var app = express()
 
-app.use(express.static("public"))
-app.listen(80)
+app.get('/', function (req, res) {
+  res.send('hello world')
+})
+
+https.createServer({
+  key: fs.readFileSync('/etc/letsencrypt/live/hordes.auction/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/hordes.auction/fullchain.pem')
+}, app)
+.listen(3000, function () {
+  console.log('Example app listening on port 3000! Go to https://localhost:3000/')
+})
