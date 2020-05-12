d9b5560080346deb3750a097f5dec42fa09650fd
Logan Henrie <loghenrie@gmail.com>
2020-05-11 19:12:35 -0500

    AHHHHHHHHHH

--- start.js
+++ start.js
@@ -1,16 +1,31 @@
-var express = require('express')
-var fs = require('fs')
-var https = require('https')
-var app = express()
+// var express = require('express')
+// var fs = require('fs')
+// var https = require('https')
+// var app = express()
 
-app.get('/', function (req, res) {
-  res.send('hello world')
-})
+// app.get('/', function (req, res) {
+//   res.send('hello world')
+// })
+
+// https.createServer({
+//   key: fs.readFileSync('/etc/letsencrypt/live/hordes.auction/privkey.pem'),
+//   cert: fs.readFileSync('/etc/letsencrypt/live/hordes.auction/fullchain.pem')
+// }, app)
+// .listen(443, function () {
+//   console.log('Example app listening on port 443! Go to https://localhost:443/')
+// })
+var express = require('express');
+var app = express();
+var https = require("https")
+
+app.get('/', function(reg, res){
+    res.sendFile(__dirname + '/public/index.html');
+});
+app.use('/client', express.static(__dirname + '/public'));
 
 https.createServer({
-  key: fs.readFileSync('/etc/letsencrypt/live/hordes.auction/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/hordes.auction/fullchain.pem')
-}, app)
-.listen(443, function () {
-  console.log('Example app listening on port 443! Go to https://localhost:443/')
-})
+    key: fs.readFileSync('/etc/letsencrypt/live/hordes.auction/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/hordes.auction/fullchain.pem')
+}, app).listen(443);
+
+console.log("Server started.");
