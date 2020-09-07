8acc832b2dcde10093597f9e1dee5922e474e057
root <root@Logan-Thinkbook.localdomain>
2020-09-07 11:35:00 -0600

    working on api

--- start.js
+++ start.js
@@ -1,33 +1,18 @@
-// var express = require('express')
-// var fs = require('fs')
-// var https = require('https')
-// var app = express()
-
-// app.get('/', function (req, res) {
-//   res.send('hello world')
-// })
-
-// https.createServer({
-//   key: fs.readFileSync('/etc/letsencrypt/live/hordes.auction/privkey.pem'),
-//   cert: fs.readFileSync('/etc/letsencrypt/live/hordes.auction/fullchain.pem')
-// }, app)
-// .listen(443, function () {
-//   console.log('Example app listening on port 443! Go to https://localhost:443/')
-// })
 var express = require('express');
 var app = express();
 var https = require("https")
 var fs = require("fs")
+var api = require ("api.js")
 
-// app.get('/', function (reg, res) {
-//     res.sendFile(__dirname + '/client/index.html');
-// });
 app.use('/', express.static(__dirname + '/client'));
+//send requests to the api module to be handled
+app.post('/api', api.post(req,res))
+app.get("/api", api.get(req,res))
 
 //force redirect to https
 var http = require('http');
 http.createServer(function (req, res) {
-    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+    res.writeHead(308, { "Location": "https://" + req.headers['host'] + req.url });
     res.end();
 }).listen(80);
 
@@ -36,5 +21,4 @@ https.createServer({
     cert: fs.readFileSync('/etc/letsencrypt/live/hordes.auction/fullchain.pem')
 }, app).listen(443);
 
-console.log("Server started.");
-//all how about now
+console.log("HTTP/S Servers started.");
