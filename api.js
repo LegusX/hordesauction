f25f69ca427b7a5c32c521f44c71ba603a44fe7a
Logan Henrie <loghenrie@gmail.com>
2020-09-08 14:03:42 -0600

    more work on google login

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 //handle all post requests
 exports.post = function(req,res) {
-    res.send("hello")
+    res.send(req.body)
 }
 exports.get = function(req,res) {
     res.send("hiya")
---
+++ client/login/index.js
@@ -0,0 +1,12 @@
+const { json } = require("express");
+
+function onSignIn(user) {
+    var id_token = googleUser.getAuthResponse().id_token;
+    fetch("/api",{
+        method: "POST",
+        body: JSON.stringify({
+            type:"login",
+            id:id_token
+        })
+    }).then((response)=>console.log(response))
+}
