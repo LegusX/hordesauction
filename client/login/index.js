d64dadf01946bdef42b17b8c6089ccb3d19026b4
Logan Henrie <loghenrie@gmail.com>
2020-09-15 13:37:55 -0600

    why me

--- client/login/index.js
+++ client/login/index.js
@@ -1,20 +1,18 @@
 function onSignIn(user) {
     var id_token = user.getAuthResponse().id_token;
-    fetch("/api",{
+    fetch("/api", {
         method: "POST",
         body: JSON.stringify({
-            type:"login",
-            id:id_token
+            type: "login",
+            id: id_token
         }),
         headers: {
             'Content-Type': 'application/json'
             // 'Content-Type': 'application/x-www-form-urlencoded',
         }
-    }).then((response)=>{
-        work = await response.text()
-        console.log(work)
-        return response.text()
-    }).then((data)=>{
-        // window.location.href = data;
-    })
+    }).then(function (response) {
+        return response.text();
+    }).then(function (data) {
+        console.log(data); // this will be a string
+    });
 }
